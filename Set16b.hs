96832ff88b978f4ec51517418495b07f1a5b5a97
josemvcerqueira <jose@ilovemochi.com>
2021-09-26 18:00:15 +0100

    feat: exercise 16b solutions

--- Set16b.hs
+++ Set16b.hs
@@ -1,16 +1,18 @@
 module Set16b where
 
-import Mooc.Todo
-import Examples.Phantom
-
 import Data.Char (toUpper)
+import Examples.Phantom
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: Define a constant pounds with type Money GBP and a value of
 -- 3. The type Money is imported from Example.Phantom but you'll need
 -- to introduce GBP yourself.
 
-pounds = todo
+data GBP
+
+pounds :: Money GBP
+pounds = Money 3
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement composition for Rates. Give composeRates a
@@ -27,7 +29,8 @@ pounds = todo
 usdToChf :: Rate USD CHF
 usdToChf = Rate 1.11
 
-composeRates rate1 rate2 = todo
+composeRates :: Rate a b -> Rate b c -> Rate a c
+composeRates (Rate x) (Rate y) = Rate $ x * y
 
 ------------------------------------------------------------------------------
 -- Ex 3: Tracking first, last and full names with phantom types. The
@@ -46,18 +49,26 @@ composeRates rate1 rate2 = todo
 --  toFirst "bob" :: Name First
 --  toLast "smith" :: Name Last
 
+data First
+
+data Last
+
+newtype Name a = Name String
 
 -- Get the String contained in a name
 --fromName :: Name a -> String
-fromName = todo
+fromName :: Name a -> String
+fromName (Name x) = x
 
 -- Build a Name First
 --toFirst :: String -> Name First
-toFirst = todo
+toFirst :: String -> Name First
+toFirst = Name
 
 -- Build a Name Last
 --toLast :: String -> Name Last
-toLast = todo
+toLast :: String -> Name Last
+toLast = Name
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the functions capitalize and toFull.
@@ -77,9 +88,14 @@ toLast = todo
 --  capitalize (toLast "smith") :: Name Last
 --  fromName (capitalize (toLast "smith")) ==> "Smith"
 
-capitalize = todo
+data Full
 
-toFull = todo
+capitalize :: Name a -> Name a
+capitalize (Name []) = Name []
+capitalize (Name (x : xs)) = Name $ toUpper x : xs
+
+toFull :: Name First -> Name Last -> Name Full
+toFull (Name x) (Name y) = Name $ x <> " " <> y
 
 ------------------------------------------------------------------------------
 -- Ex 5: Type classes can let you write code that handles different
@@ -93,3 +109,11 @@ toFull = todo
 class Render currency where
   render :: Money currency -> String
 
+instance Render EUR where
+  render (Money x) = show x <> "e"
+
+instance Render USD where
+  render (Money x) = "$" <> show x
+
+instance Render CHF where
+  render (Money x) = show x <> "chf"
